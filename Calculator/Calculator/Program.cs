a3d69a942d7094ded11bccb1cee0671346a47198
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-23 20:35:25 -0300

    funcao soma

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Soma();           
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo Valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = valor1 + valor2;
+
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine("O resultado da Soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {valor1 + valor2}");
+            Console.WriteLine("O resuldado da soma é " + (valor1 + valor2));
+
+        }
+        
+    }
+
+
+}
