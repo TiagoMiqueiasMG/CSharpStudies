feb6dbaf4e9e8b4ccfd5d914b475d628753c4a31
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-23 20:47:19 -0300

    subtraction function

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-           Soma();           
+            Subtracao();
         }
 
         static void Soma()
@@ -25,8 +25,30 @@ namespace Calculator
             Console.WriteLine($"O resultado da soma é {valor1 + valor2}");
             Console.WriteLine("O resuldado da soma é " + (valor1 + valor2));
 
+            // Usamos o ReadKey para não sair da tela
+            Console.ReadKey();
         }
-        
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            // Para ter um espacinho no terminal
+            Console.WriteLine("");
+
+            float resultado = valor1 - valor2;
+            Console.WriteLine($"O resultado da subração é: {resultado}");
+
+            // Usamos o ReadKey para não sair da tela
+            Console.ReadKey();
+        }
+
     }
 
 
