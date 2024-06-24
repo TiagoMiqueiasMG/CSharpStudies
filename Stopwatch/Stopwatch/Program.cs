5fb92afdee8347c4bd86ceda27c518eb4093a068
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-24 20:53:22 -0300

    starting counter

--- Stopwatch/Stopwatch/Program.cs
+++ Stopwatch/Stopwatch/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                // Vamos usar o console.clear para limpar a tela e mostrar
+                // só o valor atual
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+            }
+
+        }
+    }
+}
