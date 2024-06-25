e5abb45922b3f33202e2a4c4e2171524b6994488
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-25 13:59:06 -0300

    Editing the text and being able to break the line

--- TextEditor/TextEditor/Program.cs
+++ TextEditor/TextEditor/Program.cs
@@ -35,7 +35,19 @@ namespace TextEditor
 
         static void Edit()
         {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+            Console.WriteLine("------------");
+            string text = "";
 
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
         }
 
     }
