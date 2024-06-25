d18c607bc7704f3003fa5e12f4123c00e69bce68
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-24 22:35:58 -0300

    finished Stopwatch

--- Stopwatch/Stopwatch/Program.cs
+++ Stopwatch/Stopwatch/Program.cs
@@ -24,7 +24,33 @@ namespace Stopwatch
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             // Para pegar todos os caracteres menos 1(final)
             int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                // para sair da aplicação
+                System.Environment.Exit(0);
+            }
+            PreStart(time * multiplier);
+
+        }
+
+        // criando uma animação antes de começar o cronomêtro
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
         }
 
         static void Start(int time)
@@ -46,9 +72,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finished");
             Thread.Sleep(1500);
+            Menu();
         }
-
     }
-
-
 }
