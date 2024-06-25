ada3d870bab9c1ac3888e453ad3a8ab077ad5eca
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-24 21:58:12 -0300

    starting menu development

--- Stopwatch/Stopwatch/Program.cs
+++ Stopwatch/Stopwatch/Program.cs
@@ -7,7 +7,20 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start(6);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Munuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?: ");
+
+            // ToLower para converter os inputs em minúsculas
+            string data = Console.ReadLine().ToLower();
+            Console.WriteLine(data);
         }
 
         static void Start(int time)
@@ -26,11 +39,9 @@ namespace Stopwatch
                 Thread.Sleep(1000);
 
             }
-
             Console.Clear();
             Console.WriteLine("Stopwatch finished");
             Thread.Sleep(1500);
-
         }
 
     }
