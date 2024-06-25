7b0bd96f4e80d3d7e8dfe99d8d1ee66c80931763
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-25 13:26:02 -0300

    structure

--- TextEditor/TextEditor/Program.cs
+++ TextEditor/TextEditor/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace TextEditor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar um novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+
+        }
+
+        static void Open()
+        {
+
+        }
+
+        static void Edit()
+        {
+
+        }
+
     }
 }
