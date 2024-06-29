3d09b601b87e4273e32a48b1df970691ba74828f
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-28 22:37:08 -0300

    index strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -15,7 +15,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             // // Para ignorar o case sensitive use: StringComparison.OrdinalIgnoreCase
             Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
             // // Comparando com objto inválido
-            Console.WriteLine(texto.Contains(null));
+            // Console.WriteLine(texto.Contains(null));
 
             // // SartsWith/EndsWith
             // // Retorna um booleano
@@ -27,17 +27,29 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine(texto.EndsWith("testo"));
             Console.WriteLine(texto.EndsWith("xpro"));
 
-                        // Equals - ele existe em vários outros objetos do .Net
+            // Equals - ele existe em vários outros objetos do .Net
+            // Esse Equals estende para outros objetos sempre para comparar o tipo de objeto com ele mesmo do mesmo tipo
+
             // Ele compara aqui se o texto é igual ( no sentido exato)
             Console.WriteLine(texto.Equals("Este texto é um teste"));
             Console.WriteLine(texto.Equals("este texto é um teste"));
             Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
 
-            // Esse Equals estende para outros objetos sempre para comparar o tipo de objeto com ele mesmo do mesmo tipo
 
-            
+            // Index ( Posição deste item no array)
+            // Em C# vai começar do 0
+            // Nosso String é um array de caracteres
+            var textoExemplo = "Este é o texto de exemplo";
 
 
+            // IndexOf = Ele sempre vai pedir um objeto do mesmo tipo
+            // Se for uma string, tem que passar uma string
+            // Se for um inteiro, você precisa passar um inteiro
+            Console.WriteLine(textoExemplo.IndexOf("é"));
+            Console.WriteLine(textoExemplo.IndexOf("texto"));
+            // LastIndexOf
+            // último indice que ele encotrar de letra ou palavra
+            Console.WriteLine(textoExemplo.IndexOf("l"));
 
         }
     }
