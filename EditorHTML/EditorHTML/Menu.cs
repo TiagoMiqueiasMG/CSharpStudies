cd3ef33c3f4f82640d477db2f7571c1ae35bab17
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-29 23:57:43 -0300

    DrawScreen

--- EditorHTML/EditorHTML/Menu.cs
+++ EditorHTML/EditorHTML/Menu.cs
@@ -8,7 +8,40 @@ namespace EditorHTML
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            DrawScreen();
         }
-    }   
-}    
+
+        // função para desenhar a tela
+        public static void DrawScreen()
+        {
+            LineDraw();
+
+            for (int lines = 0; lines <= 5; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 50; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            LineDraw();
+        }
+        public static void LineDraw()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+
+    }
+}
