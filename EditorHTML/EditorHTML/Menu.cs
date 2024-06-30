e2bd99a0d33efb988da858337443574a6bfcd84f
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-06-30 00:09:40 -0300

    box options

--- EditorHTML/EditorHTML/Menu.cs
+++ EditorHTML/EditorHTML/Menu.cs
@@ -11,6 +11,9 @@ namespace EditorHTML
             Console.ForegroundColor = ConsoleColor.Blue;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         // função para desenhar a tela
@@ -18,7 +21,7 @@ namespace EditorHTML
         {
             LineDraw();
 
-            for (int lines = 0; lines <= 5; lines++)
+            for (int lines = 0; lines <= 10; lines++)
             {
                 Console.Write("|");
                 for (int i = 0; i <= 50; i++)
@@ -42,6 +45,26 @@ namespace EditorHTML
             Console.Write("\n");
         }
 
+        public static void WriteOptions()
+        {
+            // Para escrever algo em um lugar especifico da tela
+            // usamos o SetCursorPosition
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo ");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("1 - Abrir ");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("1 - Sair ");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+
+        }
 
     }
 }
