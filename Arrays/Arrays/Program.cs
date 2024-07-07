d245e7474c924d3c96b7245f8d764214acd64dbf
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-07-07 17:57:59 -0300

    range a array

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -11,7 +11,7 @@ namespace Arrays
             // declara como [] e sempre começa pelo 0
             // Sempre que trabalha com listas em C# é preciso usar o new
             // Pois são objetos de referência - diferente dos value types
-            var meuArray = new int[5];
+            var meuArrayEx = new int[5];
 
             // // Por estar acessando um valueType, aqui sempre vai retornar 0
             // Console.WriteLine(meuArray[0]);
@@ -26,8 +26,8 @@ namespace Arrays
             // Podemos usar modificações de ações
             // Fazendo uma assimiliação direta
             // Claro, dentro da tipagem
-            meuArray[0] = 12;
-            Console.WriteLine(meuArray[0]);
+            meuArrayEx[0] = 12;
+            Console.WriteLine(meuArrayEx[0]);
 
             // Iniciando um valor do array
             var meuArray1 = new int[5] {1, 5, 10, 15, 20};
@@ -54,6 +54,19 @@ namespace Arrays
             // Aqui não precisa especificar a quantidade de itens que serão passados no array
             int[] meuArray2 = new int[] { 1, 2, 3, 4, 5 };             
             Console.WriteLine(meuArray2[0]);
+
+            // --------------------------------------------------
+            // --------------------------------------------------
+            // Percorrendo um array
+            // Se for preciso copiar um array, é preciso clocar e se for preciso copiar os valroes use o 
+            // CopyTo
+            var meuArray = new int[5] {1, 2, 3, 4, 5};
+            
+            for(var index = 0; index < meuArray.Length; index++){
+                Console.WriteLine(meuArray[index]);
+            }
+
+
         
         }
 
