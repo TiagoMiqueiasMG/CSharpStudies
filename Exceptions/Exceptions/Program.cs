06f706c96a741b744a0441fb021393b8a6fdf7fd
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-07-14 17:53:54 -0300

    finally

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -54,6 +54,18 @@ namespace Exceptions
                 Console.WriteLine("Falha ao cadastrar texto");
             }
 
+            //CDando um catch em MinhaException 
+            catch (MinhaException ex)
+            {
+                // Chame o ex aqui e busque através do método o que deseja no retorno
+                // Usamos isso para gerar algum tipo de controle
+                // PAra ajudar a visualizar em um arquivo de texto o que aconteceu
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.QuandoAconteceu);
+                Console.WriteLine("Exceção customizada, hora que passou na exceção");
+            }
+
             // Para obter informações do erro, podemos usar o Exception e declarar um nome para ele
             catch (Exception ex)
             {
@@ -63,19 +75,42 @@ namespace Exceptions
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Ops, algo deu errado");
-            } 
-           //Criando um catch para o ArgumentNull Exception    
-        }
-        // Disparando erros
+            }
+              
+            // Finally vai ocorrer sempre 
+            // Ele não espera nenhum parâemtro
+            // Tudo que colocar no finally será executado
+            // Usado para garantir que o arquivo seja fechado, dando certo ou errado. 
+            // Interessante verificar se tem alguma conexão com o banco aberta para não deixar nada em aberto
+            finally
+            {
+                Console.WriteLine("Chegou ao fim!");
+            }
+        }      
+            
 
+        // Disparando erros
         private static void Cadastrar(string texto)
         {
             // IsNullEmpty = para chegar se o texto é nulo ou vazio
             if (string.IsNullOrEmpty(texto))
             {
                 // ArgumentNullException é um argumento para tipo nulos ou vazios
-                throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+                throw new MinhaException(DateTime.Now);
             }
+        }
+        
+
+        // Podemos criar uma Minha exception e criar uma herança de Exception
+        public class MinhaException : Exception
+        {
+            // É preciso popular ela
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+
+            public DateTime QuandoAconteceu { get; set; }
         }
     }
 }
