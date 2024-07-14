ddae0325b199779866de745a39e750e3e3f107dc
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-07-14 14:12:55 -0300

    more than one catch

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -15,7 +15,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             // Try e Catch é a base do tratamento de erros
             // use os em blocos
-            
+
             try
             {
                 for (var index = 0; index < 10; index++)
@@ -25,8 +25,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 }
 
             }
+            // Temos duas tratativas de erro, porque se a trativa der  um IndexOutOfRangeException, 
+            // vai passar na primeira tentativa, senão passará na segunda tentativa
+            // O IndexOutOfRangeException tem tudo que o exception tem, e mais outras informações
+
+            // Na nossa tratativa de erro, podemos ter vários catch's       
+            // Você sempre vai tratar seus erros do mais especifico para o mais genérico
+
+            catch (IndexOutOfRangeException ex)
+            {
+                // Chame o ex aqui e busque através do método o que deseja no retorno
+                // Usamos isso para gerar algum tipo de controle
+                // PAra ajudar a visualizar em um arquivo de texto o que aconteceu
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Não encontrei o índice na lista");
+            }
+
             // Para obter informações do erro, podemos usar o Exception e declarar um nome para ele
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 // Chame o ex aqui e busque através do método o que deseja no retorno
                 // Usamos isso para gerar algum tipo de controle
@@ -34,12 +51,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Ops, algo deu errado");
-            }
-
-
-
-
-
+            }                
         }
     }
 }
