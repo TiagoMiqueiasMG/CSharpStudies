ead7feedc50bf86c220ba0c0467816d10c8ed89c
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-07-14 13:30:10 -0300

    try and catch

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -6,7 +6,40 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            // Erros são tratados como Exception
+
+            // Exemplo            
+            var arr = new int[3];
+
+            // Try e Catch é a base do tratamento de erros
+            // use os em blocos
+            
+            try
+            {
+                for (var index = 0; index < 10; index++)
+                {
+                    // Aqui vai dar o erro: System.IndexOutOfRangeException
+                    Console.WriteLine(arr[index]);
+                }
+
+            }
+            // Para obter informações do erro, podemos usar o Exception e declarar um nome para ele
+            catch(Exception ex)
+            {
+                // Chame o ex aqui e busque através do método o que deseja no retorno
+                // Usamos isso para gerar algum tipo de controle
+                // PAra ajudar a visualizar em um arquivo de texto o que aconteceu
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado");
+            }
+
+
+
+
+
         }
     }
 }
